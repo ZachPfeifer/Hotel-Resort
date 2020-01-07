5a594e1e8a43703e6d6bb8325aa8d1cffd296ae4
Zach Pfeifer <mr.zman1@yahoo.com>
2020-01-07 11:59:21 -0700

    NPM Run Build

---
+++ build/precache-manifest.c47b456a9c32bedf8a8bbff8c95d0833.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9be11bc0b3165d7e8766490d6afe854",
+    "url": "lucia-lodge.netlify.com/index.html"
+  },
+  {
+    "revision": "e9d035660735575dc35b",
+    "url": "lucia-lodge.netlify.com/static/css/2.80c248c0.chunk.css"
+  },
+  {
+    "revision": "f3467607634cacf2a1da",
+    "url": "lucia-lodge.netlify.com/static/css/main.9f741f11.chunk.css"
+  },
+  {
+    "revision": "e9d035660735575dc35b",
+    "url": "lucia-lodge.netlify.com/static/js/2.7d4d6c34.chunk.js"
+  },
+  {
+    "revision": "3e01856dd46799eedc35ec9da08dd851",
+    "url": "lucia-lodge.netlify.com/static/js/2.7d4d6c34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3467607634cacf2a1da",
+    "url": "lucia-lodge.netlify.com/static/js/main.3d8548b0.chunk.js"
+  },
+  {
+    "revision": "a7162db0afc99dc9ff14",
+    "url": "lucia-lodge.netlify.com/static/js/runtime-main.2415200c.js"
+  },
+  {
+    "revision": "ded0a9b4852285c5b5d174f79d677053",
+    "url": "lucia-lodge.netlify.com/static/media/DefaultBcg.ded0a9b4.jpg"
+  },
+  {
+    "revision": "f448372df6b584369a4004f589ec88f5",
+    "url": "lucia-lodge.netlify.com/static/media/bg-tunnel.f448372d.jpg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "lucia-lodge.netlify.com/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "7746c06212643d70d929a957dedcc205",
+    "url": "lucia-lodge.netlify.com/static/media/logo.7746c062.svg"
+  },
+  {
+    "revision": "31cbe5cfd356429fc9db051cf40a8002",
+    "url": "lucia-lodge.netlify.com/static/media/lunch.31cbe5cf.jpg"
+  },
+  {
+    "revision": "02f8fd4bf7daa3ab38ec2a6241753cc6",
+    "url": "lucia-lodge.netlify.com/static/media/resturant.02f8fd4b.jpg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "lucia-lodge.netlify.com/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "lucia-lodge.netlify.com/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "lucia-lodge.netlify.com/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "lucia-lodge.netlify.com/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "lucia-lodge.netlify.com/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a4122b873733b2b2b4f8cc3f57ad5493",
+    "url": "lucia-lodge.netlify.com/static/media/stock-cabins-full.a4122b87.jpg"
+  }
+]);
