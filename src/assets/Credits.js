b84fee301206e505f354d4c4676bae9a2f366877
Zach Pfeifer <mr.zman1@yahoo.com>
2020-01-16 13:57:07 -0700

    Gallery Touchups

--- src/assets/Credits.js
+++ src/assets/Credits.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Credits() {
   return (
-    <p className="credits"><small>
+    <p className="credits mt-5"><small>
       Copyright Lucia Lodge, All Rights Reserved • Reservations: (866) 424-4787 • Site Design
     </small>
     </p>
