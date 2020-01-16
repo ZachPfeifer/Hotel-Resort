20d510feed48b39458c7e70cfa125c1714ba41b8
Zach Pfeifer <mr.zman1@yahoo.com>
2020-01-16 12:19:55 -0700

    Added Gallery

---
+++ src/components/Gallery.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class Gallery extends Component {
+  render() {
+    return (
+      <div className="gallery-wrapper">
+        <div className="gallery">
+          <img src="https://images7.alphacoders.com/906/thumb-350-906177.jpg" className="img img-1"></img>
+          <img src="https://images6.alphacoders.com/859/thumb-350-859506.jpg" className="img img-2"></img>
+          <img src="https://images8.alphacoders.com/943/thumb-350-943660.jpg" className="img img-3"></img>
+          <img src="https://images7.alphacoders.com/880/thumb-350-880547.jpg" className="img img-4"></img>
+          <img src="https://images2.alphacoders.com/843/thumb-350-843090.jpg" className="img img-5"></img>
+          <img src="https://images2.alphacoders.com/899/thumb-350-899247.jpg" className="img img-6"></img>
+          <img src="https://images8.alphacoders.com/881/thumb-350-881974.jpg" className="img img-7"></img>
+          <img src="https://images2.alphacoders.com/852/thumb-350-852298.jpg" className="img img-8"></img>
+          <img src="https://images6.alphacoders.com/718/thumb-350-718026.jpg" className="img img-9"></img>
+          <img src="https://images3.alphacoders.com/695/thumb-350-695172.jpg" className="img img-10"></img>
+          <img src="https://images.alphacoders.com/178/thumb-350-178211.jpg" className="img img-11"></img>
+          <img src="https://images8.alphacoders.com/740/thumb-350-740162.jpg" className="img img-12"></img>
+        </div>
+
+      </div>
+    )
+  }
+}
