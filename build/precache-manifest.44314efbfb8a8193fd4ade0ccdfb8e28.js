0a6dcce35f16cf328e9e2c4a4ca8913ca6b8f70e
Zach Pfeifer <mr.zman1@yahoo.com>
2020-01-23 16:23:53 -0700

    Re-added Build

---
+++ build/precache-manifest.44314efbfb8a8193fd4ade0ccdfb8e28.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ffd982ba066b0e6b27028fe38694ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85e0749fe58416107fbc",
+    "url": "/static/css/2.32cb66c2.chunk.css"
+  },
+  {
+    "revision": "b297bdaef33f62a24ee3",
+    "url": "/static/css/main.7058eebe.chunk.css"
+  },
+  {
+    "revision": "85e0749fe58416107fbc",
+    "url": "/static/js/2.3e92fa68.chunk.js"
+  },
+  {
+    "revision": "3e01856dd46799eedc35ec9da08dd851",
+    "url": "/static/js/2.3e92fa68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b297bdaef33f62a24ee3",
+    "url": "/static/js/main.fafe0644.chunk.js"
+  },
+  {
+    "revision": "9ad2055ddedb9a66dc13",
+    "url": "/static/js/runtime-main.844d2fb5.js"
+  },
+  {
+    "revision": "ded0a9b4852285c5b5d174f79d677053",
+    "url": "/static/media/DefaultBcg.ded0a9b4.jpg"
+  },
+  {
+    "revision": "da3e7b30cd7b8e90c175bb6ca51de4b8",
+    "url": "/static/media/Entree1.da3e7b30.jpg"
+  },
+  {
+    "revision": "f448372df6b584369a4004f589ec88f5",
+    "url": "/static/media/bg-tunnel.f448372d.jpg"
+  },
+  {
+    "revision": "5fb50b0f693d46f80b35e716d8b8f34e",
+    "url": "/static/media/dinner.5fb50b0f.jpg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "7746c06212643d70d929a957dedcc205",
+    "url": "/static/media/logo.7746c062.svg"
+  },
+  {
+    "revision": "31cbe5cfd356429fc9db051cf40a8002",
+    "url": "/static/media/lunch.31cbe5cf.jpg"
+  },
+  {
+    "revision": "e57905e2e71e4f6a632c4f7e3c587031",
+    "url": "/static/media/lunch1.e57905e2.jpg"
+  },
+  {
+    "revision": "02f8fd4bf7daa3ab38ec2a6241753cc6",
+    "url": "/static/media/resturant.02f8fd4b.jpg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a4122b873733b2b2b4f8cc3f57ad5493",
+    "url": "/static/media/stock-cabins-full.a4122b87.jpg"
+  },
+  {
+    "revision": "85236bcfa4b4c08a757023d1838f85c9",
+    "url": "/static/media/wine.85236bcf.jpg"
+  }
+]);
